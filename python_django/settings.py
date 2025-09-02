060a839d49969a6f17c8bc0382df89f86b1b3a98
E-ugine <agollaeugine@gmail.com>
2025-09-02 20:38:13 +0300

    chore:create models and API docs

--- python_django/settings.py
+++ python_django/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_api.apps.DjangoApiConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
